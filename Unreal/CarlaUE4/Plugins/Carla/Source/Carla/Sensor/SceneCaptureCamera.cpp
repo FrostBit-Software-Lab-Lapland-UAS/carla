1534424f3909792b59ec0203651657da80cf0403
Aleksi Narkilahti <83641651+AleksiNarkilahti@users.noreply.github.com>
2021-09-10 14:47:05 +0300

    Added camera post process effect to vehicle cameras

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Sensor/SceneCaptureCamera.cpp
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Sensor/SceneCaptureCamera.cpp
@@ -1,6 +1,8 @@
 // Copyright (c) 2017 Computer Vision Center (CVC) at the Universitat Autonoma
 // de Barcelona (UAB).
 //
+// Copyright(c) 2021 FrostBit Software Lab
+//
 // This work is licensed under the terms of the MIT license.
 // For a copy, see <https://opensource.org/licenses/MIT>.
 
@@ -22,6 +24,11 @@ ASceneCaptureCamera::ASceneCaptureCamera(const FObjectInitializer &ObjectInitial
 {
   AddPostProcessingMaterial(
       TEXT("Material'/Carla/PostProcessingMaterials/PhysicLensDistortion.PhysicLensDistortion'"));
+
+  // WinterSim camera lens freeze post process material effect
+  AddPostProcessingMaterial(
+      TEXT("Material'/Carla/PostProcessingMaterials/camera_effects/LensFreezeEffect.LensFreezeEffect'"));
+
 }
 
 void ASceneCaptureCamera::PostPhysTick(UWorld *World, ELevelTick TickType, float DeltaSeconds)
