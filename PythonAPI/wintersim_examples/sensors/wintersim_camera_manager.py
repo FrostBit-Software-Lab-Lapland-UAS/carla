302bd4f96e50a1c24d73a2c872ffed856ede591a
Aleksi Narkilahti <83641651+AleksiNarkilahti@users.noreply.github.com>
2022-01-12 12:34:25 +0200

    removed camera angle

--- PythonAPI/wintersim_examples/sensors/wintersim_camera_manager.py
+++ PythonAPI/wintersim_examples/sensors/wintersim_camera_manager.py
@@ -92,8 +92,7 @@ class CameraManager(object):
         self._camera_transforms = [
             (carla.Transform(carla.Location(x=-2.0*bound_x, y=+0.0*bound_y, z=2.0*bound_z), carla.Rotation(pitch=8.0)), Attachment.SpringArm),
             (carla.Transform(carla.Location(x=+0.8*bound_x, y=+0.0*bound_y, z=1.3*bound_z)), Attachment.Rigid),
-            (carla.Transform(carla.Location(x=+0.8*bound_x, y=+0.0*bound_y, z=1.3*bound_z), carla.Rotation(pitch=38.0)), Attachment.Rigid),
-            #(carla.Transform(carla.Location(x=+1.9*bound_x, y=+1.0*bound_y, z=1.2*bound_z)), Attachment.SpringArm),
+            (carla.Transform(carla.Location(x=+1.9*bound_x, y=+1.0*bound_y, z=1.2*bound_z)), Attachment.SpringArm),
             (carla.Transform(carla.Location(x=-2.8*bound_x, y=+0.0*bound_y, z=4.6*bound_z), carla.Rotation(pitch=6.0)), Attachment.SpringArm),
             (carla.Transform(carla.Location(x=-70.0, z=30.0), carla.Rotation(pitch=20.0)), Attachment.SpringArm),
             (carla.Transform(carla.Location(x=-1.0, y=-1.0*bound_y, z=0.4*bound_z)), Attachment.Rigid),]
